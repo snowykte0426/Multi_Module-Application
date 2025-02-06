4e27a553621b2b71e59495c856aa5e6f0c4e2cf7
snowykte0426 <snowykte0426@naver.com>
2025-02-06 22:51:31 +0900

    add: Implementing the view count feature

--- amond-application/src/main/kotlin/com/amond/multi/module/domain/article/domain/service/impl/GetArticleServiceImpl.kt
+++ amond-application/src/main/kotlin/com/amond/multi/module/domain/article/domain/service/impl/GetArticleServiceImpl.kt
@@ -1,17 +1,28 @@
 package com.amond.multi.module.domain.article.domain.service.impl
 
+import com.amond.multi.module.domain.article.Article
 import com.amond.multi.module.persistence.article.repository.ArticleJpaRepository
 import com.amond.multi.module.domain.article.data.dto.ArticleDto
 import com.amond.multi.module.domain.article.domain.service.GetArticleService
+import com.amond.multi.module.persistence.article.mapper.ArticleMapper
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
 open class GetArticleServiceImpl(
+    private val articleMapper: ArticleMapper,
     private val articleJpaRepository: ArticleJpaRepository
 ) : GetArticleService {
     override fun execute(): List<ArticleDto> {
-        return articleJpaRepository.findAll().map { ArticleDto(it.idx, it.content, it.viewCount) }
+        var articleJpaEntities: List<Article> = articleJpaRepository.findAll().map { articleMapper.toDomain(it).apply { viewCount++ } }
+        articleJpaRepository.saveAll(articleJpaEntities.map { articleMapper.toEntity(it) })
+        return articleJpaEntities.map {
+            ArticleDto(
+                id = it.idx!!,
+                content = it.content,
+                viewCount = it.viewCount
+            )
+        }
     }
 }
--- amond-domain/src/main/kotlin/com/amond/multi/module/domain/article/Article.kt
+++ amond-domain/src/main/kotlin/com/amond/multi/module/domain/article/Article.kt
@@ -3,5 +3,5 @@ package com.amond.multi.module.domain.article
 data class Article(
     val idx: Long?,
     val content: String,
-    val viewCount: Long
+    var viewCount: Long
 )
