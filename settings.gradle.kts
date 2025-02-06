c0a8b616fa9f4b308f69b87cfd8c5c1891385395
snowykte0426 <snowykte0426@naver.com>
2025-02-06 22:25:50 +0900

    update: Integrate into module Gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 rootProject.name = "multi.module"
 
 include("amond-presentation")
+include("amond-infrastructure")
+include("amond-domain")
+include("amond-application")
+include("amond-api")
