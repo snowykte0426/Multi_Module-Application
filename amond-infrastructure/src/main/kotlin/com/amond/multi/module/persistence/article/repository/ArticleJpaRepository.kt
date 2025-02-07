e58970a5f08b319ef04fc0905ab8f7f308ae9bf1
snowykte0426 <snowykte0426@naver.com>
2025-02-07 23:46:51 +0900

    add: Add annotation

--- amond-infrastructure/src/main/kotlin/com/amond/multi/module/persistence/article/repository/ArticleJpaRepository.kt
+++ amond-infrastructure/src/main/kotlin/com/amond/multi/module/persistence/article/repository/ArticleJpaRepository.kt
@@ -2,5 +2,7 @@ package com.amond.multi.module.persistence.article.repository
 
 import com.amond.multi.module.persistence.article.entity.ArticleJpaEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ArticleJpaRepository: JpaRepository<ArticleJpaEntity, Long>
